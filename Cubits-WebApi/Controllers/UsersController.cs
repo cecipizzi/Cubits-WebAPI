a75de0a950e8abda6a4a39ecfab3a530db20dff6
cpizzi <cecilia.pizzi@cablasociados.com>
2023-10-09 21:49:13 -0300

    se crea WEBAPI

---
+++ Cubits-WebApi/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cubits_WebApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult Get(int id)
+        {
+            return Ok($"Este es el ID:{id}");
+        }
+
+        [HttpPost]
+        [Route("CreateUsers")]
+        public IActionResult Post([FromBody] int id)
+        {
+            return Ok($"Este es el ID:{id}");
+        }
+
+        [HttpPut]
+        [Route("UpdateUser")]
+        public IActionResult Put([FromBody] int id)
+        {
+            return Ok($"Este es el ID:{id}");
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public IActionResult Deletet( int id)
+        {
+            return Ok($"Este es el ID:{id}");
+        }
+    }
+}
